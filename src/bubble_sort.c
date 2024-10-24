bbcd6ef0b2c834ba03d617819ab08072466473a5
Misaka13906 <171356928@qq.com>
2024-10-25 00:00:22 +0800

    feat: add bubble sort and heap sort

---
+++ src/bubble_sort.c
@@ -0,0 +1,14 @@
+// #include"../include/global.h"
+
+void Swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void bubbleSort(int *a, int n) {
+    for(int i=1; i<=n; i++)
+        for(int j=1; j<=n-i; j++)
+            if(a[j] > a[j+1])
+                Swap(&a[j], &a[j+1]);
+}
---
+++ src/heap_sort.c
@@ -0,0 +1,31 @@
+// #include"../include/global.h"
+
+void Swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void heapSort(int *a, int n) {
+    for(int i=2; i<=n; i++) {
+        for(int j=i; j > 1 && a[j] > a[j/2]; j /= 2) {
+            Swap(&a[j], &a[j/2]);
+        }
+    }
+    for(int i=n; i>1; i--) {
+        Swap(&a[1], &a[i]);
+        for(int j=1; j*2 < i; ) {
+            int next;
+            if(j*2+1 < i && a[j*2+1] > a[j*2]) {
+                next = j*2+1;
+            } else {
+                next = j*2;
+            }
+            if(a[next] < a[j]) {
+                break;
+            }
+            Swap(&a[j], &a[next]);
+            j = next;
+        }
+    }
+}
